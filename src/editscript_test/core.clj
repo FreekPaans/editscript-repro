c566d9e248fbc8f59cee44c2b95af61d8cc84c3d
Freek Paans <freekpaans@gmail.com>
2019-09-17 09:42:03 +0200

    test cases

--- src/editscript_test/core.clj
+++ src/editscript_test/core.clj
@@ -1,6 +1,33 @@
-(ns editscript-test.core)
+(ns editscript-test.core
+  )
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(require '[editscript.edit])
+(require '[editscript.core :as editscript])
+
+(def edit-source {})
+(def edit-dest {:x :hello-world})
+
+"this throws"
+(let [edits (editscript.edit/get-edits
+              (editscript/diff edit-source edit-dest))]
+  (editscript/patch edit-source (editscript.edit/edits->script edits))
+  )
+
+
+"the following is also weird"
+
+(assert (= (editscript.edit/get-edits
+                        (editscript/diff {} {}))
+          (editscript.edit/get-edits
+                        (editscript/diff {} {})))
+  "this succeeds"
+  )
+
+(def some-edits (editscript.edit/get-edits
+                  (editscript/diff {} {})))
+
+(assert (= some-edits (editscript.edit/get-edits
+                        (editscript/diff {} {})))
+  "this fails"
+  )
+
